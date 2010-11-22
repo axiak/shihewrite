b6ee5f50febb37a88bea0caf9a1ace393b0f62b1
Michael Axiak <mike@axiak.net>
2010-11-21 23:35:54 -0500

    Added more files.

--- settings.py
+++ settings.py
@@ -2,10 +2,14 @@
 # If you want to use a different backend you have to remove all occurences
 # of "djangoappengine" from this file.
 from djangoappengine.settings_base import *
-
 import os
 
 SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
+
+DATABASES['native'] = DATABASES['default']
+DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
+
+INTERNAL_IPS = ('127.0.0.1',)
 
 INSTALLED_APPS = (
     'djangoappengine',
@@ -14,6 +18,9 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'djangotoolbox',
+    'mediagenerator',
+    'dbindexer',
+    'debug_toolbar',
 )
 
 # This test runner captures stdout and associates tracebacks with their
@@ -33,11 +40,51 @@ ROOT_URLCONF = 'urls'
 
 SITE_ID = 1
 
-# Activate django-dbindexer if available
-try:
-    import dbindexer
-    DATABASES['native'] = DATABASES['default']
-    DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
-    INSTALLED_APPS += ('dbindexer',)
-except ImportError:
-    pass
+MIDDLWARE_CLASSES = (
+    'mediagenerator.middleware.MediaMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+# Mediagenerator
+MEDIA_DEV_MODE = False
+DEV_MEDIA_URL = '/devmedia/'
+PRODUCTION_MEDIA_URL = '/genmedia/'
+
+GLOBAL_MEDIA_DIRS = (os.path.join(os.path.dirname(__file__), 'static'),)
+
+MEDIA_BUNDLES = (
+    ('main.css',
+      'css/style.css',
+      'css/default.css',
+      'css/handheld.css',
+    ),
+    ('scripts.js',
+     'js/plugins.js',
+     'js/script.js',
+    )
+)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+ROOT_MEDIA_FILTERS = {
+    'js': 'mediagenerator.filters.yuicompressor.YUICompressor',
+    'css': 'mediagenerator.filters.yuicompressor.YUICompressor',
+}
+
+YUICOMPRESSOR_PATH = '/home/axiak/Documents/yuicompressor.jar'
+
